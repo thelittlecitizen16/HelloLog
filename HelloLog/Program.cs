77d8cf1696e686e2ad0bbb9a4e47d1c56e723c5b
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-08-31 17:51:07 +0300

    add log

---
+++ HelloLog/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Serilog;
+
+namespace HelloLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new LoggerConfiguration()
+                .WriteTo.Console().CreateLogger();
+
+            log.Information("Hello, Serilog!");
+        }
+    }
+}
